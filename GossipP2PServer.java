9503e9149fe95f928330db3f4092b77425abf794
Santiago Roig <roigs23@gmail.com>
2017-03-01 16:38:09 -0500

    Began working on interpretting messages

--- GossipP2PServer.java
+++ GossipP2PServer.java
@@ -1,15 +1,11 @@
-import gossipp2p.messages.*;
 import gossipp2p.serverthreads.*;
 import java.net.*;
-import java.io.*;
 
 public class GossipP2PServer {
 
     public static void main (String args[]) {
     	Socket sock;
-    	
-    	Message test = new GossipMessage(null, null, null);
-    	
+    	    	
     	try {
     		ServerSocket serverSocket = new ServerSocket(3333,5);
     		DatagramSocket dgSocket = new DatagramSocket(3333);
--- gossipp2p/messages/Message.java
+++ gossipp2p/messages/Message.java
@@ -2,7 +2,18 @@
 // Taking advantage of Java's polymorphism
 package gossipp2p.messages;
 
-
 public class Message {
-
+	
+	/**
+	 * Identifies message type and returns new instance based on message type.
+	 * @param strMessage String message to be identified.
+	 * @return Returns a message that has been identified by strMessage.
+	 */
+	public Message identifyMessage (String strMessage) {
+		String[] tokens = strMessage.split(":");
+		
+		
+		
+		return new Message();
+	}
 }
