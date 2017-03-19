f329b97e30142b3a97492c328b2a7781d2189a0c
santi <roigs23@gmail.com>
2017-03-19 11:06:13 -0400

    Added in android client

---
+++ GossipP2P_Client/app/src/main/java/edu/fit/santiago/gossipp2p_client/AddPeerActivity.java
@@ -0,0 +1,17 @@
+package edu.fit.santiago.gossipp2p_client;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by santiago on 3/19/17.
+ */
+
+public class AddPeerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_peer);
+    }
+
+}
---
+++ GossipP2P_Client/app/src/main/java/edu/fit/santiago/gossipp2p_client/GossipMessageActivity.java
@@ -0,0 +1,18 @@
+package edu.fit.santiago.gossipp2p_client;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by santiago on 3/18/17.
+ */
+
+public class GossipMessageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gossip_message);
+    }
+
+}
