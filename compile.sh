da9da84fe6c10c6628908650adfa18a903669b62
Xiang Li <lx3103@gmail.com>
2017-03-21 22:06:12 -0400

    Generate class files into the bin folder

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@
 
 ARG1=$1
 
-find ./src -name *.java | javac -cp ./libs/commons-cli-1.3.1.jar:./libs/sqlite-jdbc-3.16.1.jar @/dev/stdin
+find ./src -name *.java | javac -d ./bin -cp ./libs/commons-cli-1.3.1.jar:./libs/sqlite-jdbc-3.16.1.jar @/dev/stdin
 
 if [ "$ARG1" == "clean" ]; then
-    find ./src -name *.class -type f -delete
+    find ./bin -name *.class -type f -delete
 fi
